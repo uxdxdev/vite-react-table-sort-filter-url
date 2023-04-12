84b4b8aa8123ac957dd59ead35f05f57d69be200
David Morton <davidmortonmail@gmail.com>
2023-04-12 23:44:11 +0100

    mock GET return data.json

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,5 +1,10 @@
 import axios from "axios";
+import data from "../../data.json";
 
-export default axios.create({
-  baseURL: "http://localhost:3000/",
-});
+// export default axios.create({
+//   baseURL: "http://localhost:3000/",
+// });
+
+export default {
+  get: () => Promise.resolve({ data: data.candidates }),
+};
